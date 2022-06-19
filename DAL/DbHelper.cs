cbd029873e192ed7293864819ced61db8ec1fed9
Vietanh2332 <anhndv.114010121012@vtc.edu.vn>
2022-06-20 01:38:57 +0700

    Update Presetation Program file

--- DAL/DbHelper.cs
+++ DAL/DbHelper.cs
@@ -12,7 +12,7 @@ namespace DAL
             {
                 connection = new MySqlConnection
                 {
-                    ConnectionString = @"server=127.0.0.1;port=3306;user=root;password=Vietanh2302;database=Ecommerce"
+                    ConnectionString = @"server=localhost;port=3306;user=root;password=19072003;database=Ecommerce"
                 };
             }
 
