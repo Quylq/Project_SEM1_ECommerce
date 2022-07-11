de8dbc5a5050c88a8190f33751c663758c5203ee
Vietanh2332 <anhndv.114010121012@vtc.edu.vn>
2022-07-11 08:34:48 +0700

    commit

--- Presentation/BuyerPage.cs
+++ Presentation/BuyerPage.cs
@@ -1,7 +1,98 @@
-namespace Persentation
+using BL;
+
+namespace Persistence
 {
     public class BuyerPage
     {
+        private UserBL userBL;
 
+        public BuyerPage()
+        {
+            userBL = new UserBL();
+        }
+        public void CustomerPage(int ID)
+        {
+            User user = userBL.GetUserByID(ID);
+            Console.WriteLine(" --- ECOMMERCE --- ");
+            Console.WriteLine();
+            Console.WriteLine("Chào " + user.FullName);
+            Console.WriteLine();
+            Console.WriteLine("1. Đơn hàng của tôi.");
+            Console.WriteLine("2. Giỏ hàng.");
+            Console.WriteLine("3. Danh mục.");
+            Console.WriteLine("4. Tìm kiếm sản phẩm.");
+            Console.WriteLine("5. Thoát.");
+            Console.Write("Chọn: ");
+            string? choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1": 
+                    break;
+                case "2": 
+                    break;
+                case "3": 
+                    break; 
+                case "4": 
+                    break;
+                case "5": 
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Vui lòng chọn 1, 2, 3, 4 hoặc 5!");
+                    CustomerPage(user.UserId);
+                    break;
+            }
+        }
+        public void Cart(int ID)
+        {
+            User user = userBL.GetUserByID(ID);
+            Console.WriteLine("1. Loại bỏ khỏi giỏ hàng.");
+            Console.WriteLine("2. Thanh Toán.");
+            Console.WriteLine("3. Quay lại.");
+            Console.Write("Chọn: ");
+            string? choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1": 
+                    break; 
+                case "2": 
+                    break;
+                case "3":
+                    CustomerPage(user.UserId);
+                    break;
+                default:
+                    Console.WriteLine("Vui lòng chọn 1, 2 hoặc 3!");
+                    Cart(user.UserId);
+                    break;
+            }
+        }
+        public void ShowCategory(int ID)
+        {
+            User user = userBL.GetUserByID(ID);
+            Console.WriteLine("1. Xem thông tin sản phẩm.");
+            Console.WriteLine("2. Quay lại.");
+            Console.Write("Chọn: ");
+            string? choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1": 
+                    break;
+                case "2":
+                    CustomerPage(user.UserId);
+                    break; 
+                default:
+                    Console.WriteLine("Vui lòng chọn 1 hoặc 2!");
+                    ShowCategory(user.UserId);
+                    break;
+            }
+        }
+        public void SearchProduct()
+        {
+            
+        }
+        public void Myorder()
+        {
+            
+        }
     }
 }
--- Presentation/Ecommerce.cs
+++ Presentation/Ecommerce.cs
@@ -7,10 +7,12 @@ namespace Persistence
     public class Ecommerce
     {
         private UserBL userBL;
+        private BuyerPage buyerPage;
 
         public Ecommerce()
         {
             userBL = new UserBL();
+            buyerPage = new BuyerPage();
         }
 
         public void Login()
@@ -35,7 +37,7 @@ namespace Persistence
                     }
                     else if (user.Role == "Customer")
                     {
-                        CustomerPage(user.UserId);
+                        buyerPage.CustomerPage(user.UserId);
                     }
                     else
                     {
@@ -55,7 +57,10 @@ namespace Persistence
         public void SellerPage (int ID)
         {
             User user = userBL.GetUserByID(ID);
+            Console.WriteLine(" --- ECOMMERCE --- ");
+            Console.WriteLine();
             Console.WriteLine("Chào " + user.FullName);
+            Console.WriteLine();
             Console.WriteLine("1. Quản lý đơn đặt hàng.");
             Console.WriteLine("2. Danh sách sản phẩm.");
             Console.WriteLine("3. Quản lý danh mục sản phẩm.");
@@ -79,36 +84,6 @@ namespace Persistence
                 default:
                     Console.WriteLine("Vui lòng chọn 1, 2, 3 hoặc 4!");
                     SellerPage (user.UserId);
-                    break;
-            }
-        }
-
-        public void CustomerPage(int ID)
-        {
-            User user = userBL.GetUserByID(ID);
-            Console.WriteLine("1. Đơn hàng của tôi.");
-            Console.WriteLine("2. Giỏ hàng.");
-            Console.WriteLine("3. Danh mục.");
-            Console.WriteLine("4. Tìm kiếm sản phẩm.");
-            Console.WriteLine("5. Thoát.");
-            Console.Write("Chọn: ");
-            string? choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "1": 
-                    break;
-                case "2": 
-                    break;
-                case "3": 
-                    break; 
-                case "4": 
-                    break;
-                case "5": 
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Vui lòng chọn 1, 2, 3, 4 hoặc 5!");
-                    CustomerPage(user.UserId);
                     break;
             }
         }
@@ -177,57 +152,6 @@ namespace Persistence
                     CategoryManagement(user.UserId);
                     break;
             }
-        }
-        public void Cart(int ID)
-        {
-            User user = userBL.GetUserByID(ID);
-            Console.WriteLine("1. Loại bỏ khỏi giỏ hàng.");
-            Console.WriteLine("2. Thanh Toán.");
-            Console.WriteLine("3. Quay lại.");
-            Console.Write("Chọn: ");
-            string? choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "1": 
-                    break; 
-                case "2": 
-                    break;
-                case "3":
-                    CustomerPage(user.UserId);
-                    break;
-                default:
-                    Console.WriteLine("Vui lòng chọn 1, 2 hoặc 3!");
-                    Cart(user.UserId);
-                    break;
-            }
-        }
-        public void ShowCategory(int ID)
-        {
-            User user = userBL.GetUserByID(ID);
-            Console.WriteLine("1. Xem thông tin sản phẩm.");
-            Console.WriteLine("2. Quay lại.");
-            Console.Write("Chọn: ");
-            string? choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "1": 
-                    break;
-                case "2":
-                    CustomerPage(user.UserId);
-                    break; 
-                default:
-                    Console.WriteLine("Vui lòng chọn 1 hoặc 2!");
-                    ShowCategory(user.UserId);
-                    break;
-            }
-        }
-        public void SearchProduct()
-        {
-            
-        }
-        public void Myorder()
-        {
-            
-        }
+        }   
     }
 }
